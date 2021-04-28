93702c5e24d5221cd3f7fe92269495bb7c7cc2b6
Marcin Kmiecik <sggp.kmiecik@gmail.com>
2021-04-28 12:10:54 +0200

    Added scenarios with steps

--- RestSharpQuiz/Features/RegistrationForm.feature
+++ RestSharpQuiz/Features/RegistrationForm.feature
@@ -10,4 +10,55 @@ Scenario: REGISTRATION_FORM_[/quiz/users/register]_[POST]_2_send_request_with_em
 	Given User didn't fill data
 	When Request sends to API 
 	Then The server should return status 400
-	And JSON body with message about missing data
+	And Response with error about missing data
+
+Scenario Outline: REGISTRATION_FORM_[/quiz/users/register]_[POST]_3_send_request_with_incorrect_<field>
+	Given Users filled data with incorrect <field>
+	When Request sends to API
+	Then The server should return status 400
+	And Response with error about incorrect <field>
+
+Examples: 
+	| field       |
+	| Adres email |
+	| Hasło       |
+	| Płeć        |
+	| Weryfikacja |
+
+Scenario Outline: REGISTRATION_FORM_[/quiz/users/register]_[POST]_4_send_request_without_data_in_<field>
+	Given User didn't fill data in <field>
+	When Request sends to API
+	Then The server should return status 400
+	And Response with error about empty <field>
+
+Examples: 
+	| field         |
+	| Adres email   |
+	| Hasło         |
+	| Powtórz hasło |
+	| Płeć			|
+	| Weryfikacja	|
+
+Scenario Outline: REGISTRATION_FORM_[/quiz/users/register]_[POST]_5_send_request_with_data_which_is_too_short
+	Given User filled too short <field>
+	When Request sends to API
+	Then The server should return status 400
+	And Response with error about too short <field>
+
+Examples: 
+	| field       |
+	| Adres email |
+	| Hasło       |
+	| Płeć        |
+
+Scenario Outline: REGISTRATION_FORM_[/quiz/users/register]_[POST]_6_send_request_with_data_which_is_too_long
+	Given User filled too long <field>
+	When Request sends to API
+	Then The server should return status 400
+	And Response with error about too long <field>
+
+Examples: 
+	| field       |
+	| Adres email |
+	| Hasło       |
+	| Płeć        |
---
+++ RestSharpQuiz/Steps/RegistrationFormSteps.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace RestSharpQuiz.Steps
+{
+    [Binding]
+    public class RegistrationFormSteps
+    {
+        [Given(@"User filled data correctly")]
+        public void GivenUserFilledDataCorrectly()
+        {
+            
+        }
+
+        [Given(@"User didn't fill data")]
+        public void GivenUserDidnTFillData()
+        {
+            
+        }
+
+        [Given(@"Users filled data with incorrect Adres email")]
+        public void GivenUsersFilledDataWithIncorrectAdresEmail()
+        {
+            
+        }
+
+        [Given(@"Users filled data with incorrect Hasło")]
+        public void GivenUsersFilledDataWithIncorrectHaslo()
+        {
+            
+        }
+
+        [Given(@"Users filled data with incorrect Płeć")]
+        public void GivenUsersFilledDataWithIncorrectPlec()
+        {
+            
+        }
+
+        [Given(@"Users filled data with incorrect Weryfikacja")]
+        public void GivenUsersFilledDataWithIncorrectWeryfikacja()
+        {
+            
+        }
+
+        [Given(@"User didn't fill data in Adres email")]
+        public void GivenUserDidnTFillDataInAdresEmail()
+        {
+            
+        }
+
+        [Given(@"User didn't fill data in Hasło")]
+        public void GivenUserDidnTFillDataInHaslo()
+        {
+            
+        }
+
+        [Given(@"User didn't fill data in Powtórz hasło")]
+        public void GivenUserDidnTFillDataInPowtorzHaslo()
+        {
+            
+        }
+
+        [Given(@"User didn't fill data in Płeć")]
+        public void GivenUserDidnTFillDataInPlec()
+        {
+            
+        }
+
+        [Given(@"User didn't fill data in Weryfikacja")]
+        public void GivenUserDidnTFillDataInWeryfikacja()
+        {
+            
+        }
+
+        [Given(@"User filled too short Adres email")]
+        public void GivenUserFilledTooShortAdresEmail()
+        {
+            
+        }
+
+        [Given(@"User filled too short Hasło")]
+        public void GivenUserFilledTooShortHaslo()
+        {
+            
+        }
+
+        [Given(@"User filled too short Płeć")]
+        public void GivenUserFilledTooShortPlec()
+        {
+            
+        }
+
+        [Given(@"User filled too long Adres email")]
+        public void GivenUserFilledTooLongAdresEmail()
+        {
+            
+        }
+
+        [Given(@"User filled too long Hasło")]
+        public void GivenUserFilledTooLongHaslo()
+        {
+            
+        }
+
+        [Given(@"User filled too long Płeć")]
+        public void GivenUserFilledTooLongPlec()
+        {
+            
+        }
+
+        [When(@"Request sends to API")]
+        public void WhenRequestSendsToAPI()
+        {
+            
+        }
+        
+        [Then(@"The server should return positive status 200")]
+        public void ThenTheServerShouldReturnPositiveStatus()
+        {
+            
+        }
+        
+        [Then(@"Response with message about successfully process")]
+        public void ThenResponseWithMessageAboutSuccessfullyProcess()
+        {
+            
+        }
+
+        [Then(@"The server should return status 400")]
+        public void ThenTheServerShouldReturnStatus()
+        {
+            
+        }
+
+        [Then(@"Response with error about missing data")]
+        public void ThenResponseWithErrorAboutMissingData()
+        {
+            
+        }
+
+        [Then(@"Response with error about incorrect Adres email")]
+        public void ThenResponseWithErrorAboutIncorrectAdresEmail()
+        {
+            
+        }
+
+        [Then(@"Response with error about incorrect Hasło")]
+        public void ThenResponseWithErrorAboutIncorrectHaslo()
+        {
+            
+        }
+
+        [Then(@"Response with error about incorrect Płeć")]
+        public void ThenResponseWithErrorAboutIncorrectPlec()
+        {
+            
+        }
+
+        [Then(@"Response with error about incorrect Weryfikacja")]
+        public void ThenResponseWithErrorAboutIncorrectWeryfikacja()
+        {
+            
+        }
+
+        [Then(@"Response with error about empty Adres email")]
+        public void ThenResponseWithErrorAboutEmptyAdresEmail()
+        {
+            
+        }
+
+        [Then(@"Response with error about empty Hasło")]
+        public void ThenResponseWithErrorAboutEmptyHaslo()
+        {
+            
+        }
+
+        [Then(@"Response with error about empty Powtórz hasło")]
+        public void ThenResponseWithErrorAboutEmptyPowtorzHaslo()
+        {
+            
+        }
+
+        [Then(@"Response with error about empty Płeć")]
+        public void ThenResponseWithErrorAboutEmptyPlec()
+        {
+            
+        }
+
+        [Then(@"Response with error about empty Weryfikacja")]
+        public void ThenResponseWithErrorAboutEmptyWeryfikacja()
+        {
+            
+        }
+
+        [Then(@"Response with error about too short Adres email")]
+        public void ThenResponseWithErrorAboutTooShortAdresEmail()
+        {
+            
+        }
+
+        [Then(@"Response with error about too short Hasło")]
+        public void ThenResponseWithErrorAboutTooShortHaslo()
+        {
+            
+        }
+
+        [Then(@"Response with error about too short Płeć")]
+        public void ThenResponseWithErrorAboutTooShortPlec()
+        {
+            
+        }
+
+        [Then(@"Response with error about too long Adres email")]
+        public void ThenResponseWithErrorAboutTooLongAdresEmail()
+        {
+            
+        }
+
+        [Then(@"Response with error about too long Hasło")]
+        public void ThenResponseWithErrorAboutTooLongHaslo()
+        {
+            
+        }
+
+        [Then(@"Response with error about too long Płeć")]
+        public void ThenResponseWithErrorAboutTooLongPlec()
+        {
+            
+        }
+    }
+}
