c33787a2cfab16f06360950b18a010247767af94
Marcin Kmiecik <sggp.kmiecik@gmail.com>
2022-01-23 16:46:16 +0100

    Issue-13 (#14)

--- RestSharpQuiz/Features/ChangeUserEmail.feature.cs
+++ RestSharpQuiz/Features/ChangeUserEmail.feature.cs
@@ -78,23 +78,20 @@ namespace RestSharpQuiz.Features
 #line 3
 #line hidden
 #line 4
- testRunner.Given("User signs up in system", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
-#line hidden
-#line 5
- testRunner.And("User signs in", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+ testRunner.Given("User registers into system and logs in", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
 #line hidden
         }
         
         [NUnit.Framework.TestAttribute()]
-        [NUnit.Framework.DescriptionAttribute("CHANGE_USER_EMAIL_1_[/quiz/users/email]_[PUT]_send_request_with_correct_email_and" +
-            "_password")]
-        public virtual void CHANGE_USER_EMAIL_1_QuizUsersEmail_PUT_Send_Request_With_Correct_Email_And_Password()
+        [NUnit.Framework.DescriptionAttribute("CHANGE_USER_EMAIL_[/quiz/users/email]_[PATCH]_1_send_request_with_correct_email_a" +
+            "nd_password")]
+        public virtual void CHANGE_USER_EMAIL_QuizUsersEmail_PATCH_1_Send_Request_With_Correct_Email_And_Password()
         {
             string[] tagsOfScenario = ((string[])(null));
             System.Collections.Specialized.OrderedDictionary argumentsOfScenario = new System.Collections.Specialized.OrderedDictionary();
-            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("CHANGE_USER_EMAIL_1_[/quiz/users/email]_[PUT]_send_request_with_correct_email_and" +
-                    "_password", null, tagsOfScenario, argumentsOfScenario, this._featureTags);
-#line 7
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("CHANGE_USER_EMAIL_[/quiz/users/email]_[PATCH]_1_send_request_with_correct_email_a" +
+                    "nd_password", null, tagsOfScenario, argumentsOfScenario, this._featureTags);
+#line 6
 this.ScenarioInitialize(scenarioInfo);
 #line hidden
             bool isScenarioIgnored = default(bool);
@@ -117,32 +114,32 @@ this.ScenarioInitialize(scenarioInfo);
 #line 3
 this.FeatureBackground();
 #line hidden
-#line 8
+#line 7
  testRunner.Given("User filled correctly data", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
 #line hidden
-#line 9
+#line 8
  testRunner.When("Request sends to API", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
 #line hidden
-#line 10
+#line 9
  testRunner.Then("The server should return status 200", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
 #line hidden
-#line 11
- testRunner.And("Response with success message", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 10
+ testRunner.And("Response with a new token", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
             }
             this.ScenarioCleanup();
         }
         
         [NUnit.Framework.TestAttribute()]
-        [NUnit.Framework.DescriptionAttribute("CHANGE_USER_EMAIL_2_[/quiz/users/email]_[PUT]_send_request_with_email_which_is_as" +
-            "signed_to_account")]
-        public virtual void CHANGE_USER_EMAIL_2_QuizUsersEmail_PUT_Send_Request_With_Email_Which_Is_Assigned_To_Account()
+        [NUnit.Framework.DescriptionAttribute("CHANGE_USER_EMAIL_[/quiz/users/email]_[PATCH]_2_send_request_with_email_which_is_" +
+            "assigned_to_account")]
+        public virtual void CHANGE_USER_EMAIL_QuizUsersEmail_PATCH_2_Send_Request_With_Email_Which_Is_Assigned_To_Account()
         {
             string[] tagsOfScenario = ((string[])(null));
             System.Collections.Specialized.OrderedDictionary argumentsOfScenario = new System.Collections.Specialized.OrderedDictionary();
-            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("CHANGE_USER_EMAIL_2_[/quiz/users/email]_[PUT]_send_request_with_email_which_is_as" +
-                    "signed_to_account", null, tagsOfScenario, argumentsOfScenario, this._featureTags);
-#line 13
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("CHANGE_USER_EMAIL_[/quiz/users/email]_[PATCH]_2_send_request_with_email_which_is_" +
+                    "assigned_to_account", null, tagsOfScenario, argumentsOfScenario, this._featureTags);
+#line 12
 this.ScenarioInitialize(scenarioInfo);
 #line hidden
             bool isScenarioIgnored = default(bool);
@@ -165,16 +162,16 @@ this.ScenarioInitialize(scenarioInfo);
 #line 3
 this.FeatureBackground();
 #line hidden
-#line 14
+#line 13
  testRunner.Given("User filled email which is assigned to account", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
 #line hidden
-#line 15
+#line 14
  testRunner.When("Request sends to API", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
 #line hidden
-#line 16
+#line 15
  testRunner.Then("The server should return status 400", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
 #line hidden
-#line 17
+#line 16
  testRunner.And("Response with message about already assigned email", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
             }
@@ -182,15 +179,15 @@ this.FeatureBackground();
         }
         
         [NUnit.Framework.TestAttribute()]
-        [NUnit.Framework.DescriptionAttribute("CHANGE_USER_EMAIL_3_[/quiz/users/email]_[PUT]_send_request_with_password_which_is" +
-            "_not_assigned_to_account")]
-        public virtual void CHANGE_USER_EMAIL_3_QuizUsersEmail_PUT_Send_Request_With_Password_Which_Is_Not_Assigned_To_Account()
+        [NUnit.Framework.DescriptionAttribute("CHANGE_USER_EMAIL_[/quiz/users/email]_[PATCH]_3_send_request_with_password_which_" +
+            "is_not_assigned_to_account")]
+        public virtual void CHANGE_USER_EMAIL_QuizUsersEmail_PATCH_3_Send_Request_With_Password_Which_Is_Not_Assigned_To_Account()
         {
             string[] tagsOfScenario = ((string[])(null));
             System.Collections.Specialized.OrderedDictionary argumentsOfScenario = new System.Collections.Specialized.OrderedDictionary();
-            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("CHANGE_USER_EMAIL_3_[/quiz/users/email]_[PUT]_send_request_with_password_which_is" +
-                    "_not_assigned_to_account", null, tagsOfScenario, argumentsOfScenario, this._featureTags);
-#line 19
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("CHANGE_USER_EMAIL_[/quiz/users/email]_[PATCH]_3_send_request_with_password_which_" +
+                    "is_not_assigned_to_account", null, tagsOfScenario, argumentsOfScenario, this._featureTags);
+#line 18
 this.ScenarioInitialize(scenarioInfo);
 #line hidden
             bool isScenarioIgnored = default(bool);
@@ -213,16 +210,16 @@ this.ScenarioInitialize(scenarioInfo);
 #line 3
 this.FeatureBackground();
 #line hidden
-#line 20
+#line 19
  testRunner.Given("User filled password which is incorrect", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
 #line hidden
-#line 21
+#line 20
  testRunner.When("Request sends to API", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
 #line hidden
-#line 22
+#line 21
  testRunner.Then("The server should return status 400", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
 #line hidden
-#line 23
+#line 22
  testRunner.And("Response with message about password", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
             }
@@ -230,19 +227,21 @@ this.FeatureBackground();
         }
         
         [NUnit.Framework.TestAttribute()]
-        [NUnit.Framework.DescriptionAttribute("CHANGE_USER_EMAIL_4_[/quiz/users/email]_[PUT]_send_request_with_incorrect_email")]
+        [NUnit.Framework.DescriptionAttribute("CHANGE_USER_EMAIL_[/quiz/users/email]_[PATCH]_4_send_request_with_incorrect_email" +
+            "")]
         [NUnit.Framework.TestCaseAttribute("without @", "exmapleEmail.com", null)]
-        [NUnit.Framework.TestCaseAttribute("which is too long", @"xv5XfZ1LXURRkaFvIEvzp7j8Fuj16dziBW9Pv8quGJsdQfOnyKV6hosAlndp2Au244iHlJeHIaQHx2rqzcpyiwjqDywrzFz6CgCvUVVVngr2IkTfDQBsB88llpJYJWY2xbOdvLIBXQ2QOM65PlCBp0TTVQX0lBvFLIAZg7kZNM2hQIN3bpvQ2GaacERotQuF3JPwlvUUr84B9h81Y4z0MmP1hrz1bDaoAzlU5jJx3ft9dCJLXUMUgig4rDDOv@email.com", null)]
+        [NUnit.Framework.TestCaseAttribute("too long", @"xv5XfZ1LXURRkaFvIEvzp7j8Fuj16dziBW9Pv8quGJsdQfOnyKV6hosAlndp2Au244iHlJeHIaQHx2rqzcpyiwjqDywrzFz6CgCvUVVVngr2IkTfDQBsB88llpJYJWY2xbOdvLIBXQ2QOM65PlCBp0TTVQX0lBvFLIAZg7kZNM2hQIN3bpvQ2GaacERotQuF3JPwlvUUr84B9h81Y4z0MmP1hrz1bDaoAzlU5jJx3ft9dCJLXUMUgig4rDDOv@email.com", null)]
         [NUnit.Framework.TestCaseAttribute("without data after @", "exmapleEmail@", null)]
         [NUnit.Framework.TestCaseAttribute("without data", "empty", null)]
-        public virtual void CHANGE_USER_EMAIL_4_QuizUsersEmail_PUT_Send_Request_With_Incorrect_Email(string typeOfMistake, string data, string[] exampleTags)
+        public virtual void CHANGE_USER_EMAIL_QuizUsersEmail_PATCH_4_Send_Request_With_Incorrect_Email(string typeOfMistake, string data, string[] exampleTags)
         {
             string[] tagsOfScenario = exampleTags;
             System.Collections.Specialized.OrderedDictionary argumentsOfScenario = new System.Collections.Specialized.OrderedDictionary();
             argumentsOfScenario.Add("type of mistake", typeOfMistake);
             argumentsOfScenario.Add("data", data);
-            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("CHANGE_USER_EMAIL_4_[/quiz/users/email]_[PUT]_send_request_with_incorrect_email", null, tagsOfScenario, argumentsOfScenario, this._featureTags);
-#line 25
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("CHANGE_USER_EMAIL_[/quiz/users/email]_[PATCH]_4_send_request_with_incorrect_email" +
+                    "", null, tagsOfScenario, argumentsOfScenario, this._featureTags);
+#line 24
 this.ScenarioInitialize(scenarioInfo);
 #line hidden
             bool isScenarioIgnored = default(bool);
@@ -265,39 +264,37 @@ this.ScenarioInitialize(scenarioInfo);
 #line 3
 this.FeatureBackground();
 #line hidden
-#line 26
+#line 25
  testRunner.Given(string.Format("User filled email \'{0}\' with \'{1}\'", typeOfMistake, data), ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
 #line hidden
-#line 27
+#line 26
  testRunner.When("Request sends to API", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
 #line hidden
-#line 28
+#line 27
  testRunner.Then("The server should return status 400", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
 #line hidden
-#line 29
- testRunner.And("Response with message about incorrect email", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 28
+ testRunner.And(string.Format("Response with message about incorrect email based on {0}", typeOfMistake), ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
             }
             this.ScenarioCleanup();
         }
         
         [NUnit.Framework.TestAttribute()]
-        [NUnit.Framework.DescriptionAttribute("CHANGE_USER_EMAIL_5_[/quiz/users/email]_[PUT]_send_request_with_incorrect_passwor" +
-            "d")]
+        [NUnit.Framework.DescriptionAttribute("CHANGE_USER_EMAIL_[/quiz/users/email]_[PATCH]_5_send_request_with_incorrect_passw" +
+            "ord")]
         [NUnit.Framework.TestCaseAttribute("which is too short", "short", null)]
         [NUnit.Framework.TestCaseAttribute("which is too long", "userEnteredTooLongPassword@WhichIsWrong", null)]
         [NUnit.Framework.TestCaseAttribute("without data", "empty", null)]
-        [NUnit.Framework.TestCaseAttribute("without special key", "password", null)]
-        [NUnit.Framework.TestCaseAttribute("which is not a string", "float", null)]
-        public virtual void CHANGE_USER_EMAIL_5_QuizUsersEmail_PUT_Send_Request_With_Incorrect_Password(string typeOfMistake, string data, string[] exampleTags)
+        public virtual void CHANGE_USER_EMAIL_QuizUsersEmail_PATCH_5_Send_Request_With_Incorrect_Password(string typeOfMistake, string data, string[] exampleTags)
         {
             string[] tagsOfScenario = exampleTags;
             System.Collections.Specialized.OrderedDictionary argumentsOfScenario = new System.Collections.Specialized.OrderedDictionary();
             argumentsOfScenario.Add("type of mistake", typeOfMistake);
             argumentsOfScenario.Add("data", data);
-            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("CHANGE_USER_EMAIL_5_[/quiz/users/email]_[PUT]_send_request_with_incorrect_passwor" +
-                    "d", null, tagsOfScenario, argumentsOfScenario, this._featureTags);
-#line 38
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("CHANGE_USER_EMAIL_[/quiz/users/email]_[PATCH]_5_send_request_with_incorrect_passw" +
+                    "ord", null, tagsOfScenario, argumentsOfScenario, this._featureTags);
+#line 37
 this.ScenarioInitialize(scenarioInfo);
 #line hidden
             bool isScenarioIgnored = default(bool);
@@ -320,17 +317,65 @@ this.ScenarioInitialize(scenarioInfo);
 #line 3
 this.FeatureBackground();
 #line hidden
-#line 39
+#line 38
  testRunner.Given(string.Format("User filled password \'{0}\' with \'{1}\'", typeOfMistake, data), ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
 #line hidden
-#line 40
+#line 39
  testRunner.When("Request sends to API", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
 #line hidden
-#line 41
+#line 40
  testRunner.Then("The server should return status 400", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
 #line hidden
-#line 42
- testRunner.And("Response with message about incorrect password", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 41
+ testRunner.And(string.Format("Response with message about incorrect password based on {0}", typeOfMistake), ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+            }
+            this.ScenarioCleanup();
+        }
+        
+        [NUnit.Framework.TestAttribute()]
+        [NUnit.Framework.DescriptionAttribute("CHANGE_USER_EMAIL_[/quiz/users/email]_[PATCH]_6_send_request_without_authorizatio" +
+            "n")]
+        public virtual void CHANGE_USER_EMAIL_QuizUsersEmail_PATCH_6_Send_Request_Without_Authorization()
+        {
+            string[] tagsOfScenario = ((string[])(null));
+            System.Collections.Specialized.OrderedDictionary argumentsOfScenario = new System.Collections.Specialized.OrderedDictionary();
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("CHANGE_USER_EMAIL_[/quiz/users/email]_[PATCH]_6_send_request_without_authorizatio" +
+                    "n", null, tagsOfScenario, argumentsOfScenario, this._featureTags);
+#line 49
+this.ScenarioInitialize(scenarioInfo);
+#line hidden
+            bool isScenarioIgnored = default(bool);
+            bool isFeatureIgnored = default(bool);
+            if ((tagsOfScenario != null))
+            {
+                isScenarioIgnored = tagsOfScenario.Where(__entry => __entry != null).Where(__entry => String.Equals(__entry, "ignore", StringComparison.CurrentCultureIgnoreCase)).Any();
+            }
+            if ((this._featureTags != null))
+            {
+                isFeatureIgnored = this._featureTags.Where(__entry => __entry != null).Where(__entry => String.Equals(__entry, "ignore", StringComparison.CurrentCultureIgnoreCase)).Any();
+            }
+            if ((isScenarioIgnored || isFeatureIgnored))
+            {
+                testRunner.SkipScenario();
+            }
+            else
+            {
+                this.ScenarioStart();
+#line 3
+this.FeatureBackground();
+#line hidden
+#line 50
+ testRunner.Given("User filled correctly data without authorization", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
+#line hidden
+#line 51
+ testRunner.When("Request sends to API", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line hidden
+#line 52
+ testRunner.Then("The server should return status 403", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line hidden
+#line 53
+ testRunner.And("Response with error message about authorization", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
             }
             this.ScenarioCleanup();
---
+++ RestSharpQuiz/Steps/ChangeUserEmailSteps.cs
@@ -0,0 +1,166 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using RestSharp;
+using RestSharpQuiz.Models;
+using System;
+using TechTalk.SpecFlow;
+
+namespace RestSharpQuiz.Steps
+{
+    [Binding]
+    [Scope(Feature = "Change user email")]
+    public class ChangeUserEmailSteps
+    {
+        private RestClient _restClinet;
+        private RestRequest _restRequest;
+        private RestResponse _restResponse;
+        private User user;
+        private Response response;
+        private string updateUserEmail = "/users/email";
+        private string userToken;
+        private string envUrl = TestContext.Parameters["entryEndpoint"];
+
+        public ChangeUserEmailSteps(RestClient restClinet)
+        {
+            _restClinet = new RestClient(restClinet.BaseUrl + updateUserEmail);
+            _restRequest = new RestRequest(Method.PATCH);
+            user = new User(null, null, null, null, false);
+        }
+
+        [Given(@"User registers into system and logs in")]
+        public void GivenUserRegistersIntoSystem()
+        {
+            RestClient restClinet = new RestClient();
+            RestResponse restResponse = new RestResponse();
+
+            if (string.IsNullOrEmpty(envUrl))
+                envUrl = "https://learnandtest.herokuapp.com/quiz";
+
+            RestRequest restRequest = new RestRequest(envUrl + "/users/register", Method.POST);
+            user = user.CreateUser(user);
+
+            restRequest.AddParameter("application/json", JsonConvert.SerializeObject(user), ParameterType.RequestBody);
+
+            restResponse = (RestResponse)_restClinet.Execute(restRequest);
+            Assert.AreEqual(200, (int)restResponse.StatusCode);
+
+            restRequest = new RestRequest(envUrl + "/users/login", Method.POST);
+
+            restRequest.AddParameter("userEmail", user.userEmail);
+            restRequest.AddParameter("userPassword", user.userPassword);
+
+            restResponse = (RestResponse)_restClinet.Execute(restRequest);
+            response = JsonConvert.DeserializeObject<Response>(restResponse.Content);
+            Assert.AreEqual(200, (int)restResponse.StatusCode);
+
+            userToken = response.token;
+        }
+        
+        [Given(@"User filled correctly data")]
+        public void GivenUserFilledCorrectlyData()
+        {
+            _restRequest.AddHeader("Authorization", "Bearer " + userToken);
+            _restRequest.AddParameter("newUserEmail", user.userEmail);
+            _restRequest.AddParameter("userPassword", user.userPassword);
+        }
+        
+        [Given(@"User filled email which is assigned to account")]
+        public void GivenUserFilledEmailWhichIsAssignedToAccount()
+        {
+            _restRequest.AddHeader("Authorization", "Bearer " + userToken);
+            _restRequest.AddParameter("newUserEmail", user.GenerateEmail());
+            _restRequest.AddParameter("userPassword", user.userPassword);
+        }
+        
+        [Given(@"User filled password which is incorrect")]
+        public void GivenUserFilledPasswordWhichIsIncorrect()
+        {
+            _restRequest.AddHeader("Authorization", "Bearer " + userToken);
+            _restRequest.AddParameter("newUserEmail", user.userEmail);
+            _restRequest.AddParameter("userPassword", "passwd");
+        }
+        
+        [Given(@"User filled email '(.*)' with '(.*)'")]
+        public void GivenUserFilledEmailWith(string kindOfMistake, string data)
+        {
+            _restRequest.AddHeader("Authorization", "Bearer " + userToken);
+            _restRequest.AddParameter("newUserEmail", data);
+            _restRequest.AddParameter("userPassword", user.userPassword);
+        }
+        
+        [Given(@"User filled password '(.*)' with '(.*)'")]
+        public void GivenUserFilledPasswordWith(string kindOfMistake, string data)
+        {
+            _restRequest.AddHeader("Authorization", "Bearer " + userToken);
+            _restRequest.AddParameter("newUserEmail", user.userEmail);
+            _restRequest.AddParameter("userPassword", data);
+        }
+
+        [Given(@"User filled correctly data without authorization")]
+        public void GivenUserFilledCorrectlyDataWithoutAuthorization()
+        {
+            _restRequest.AddParameter("newUserEmail", user.userEmail);
+            _restRequest.AddParameter("userPassword", user.userPassword);
+        }
+
+        [When(@"Request sends to API")]
+        public void WhenRequestSendsToAPI()
+        {
+            _restResponse = (RestResponse)_restClinet.Execute(_restRequest);
+        }
+        
+        [Then(@"The server should return status (.*)")]
+        public void ThenTheServerShouldReturnStatus(int statusCode)
+        {
+            Assert.AreEqual(statusCode, (int)_restResponse.StatusCode);
+        }
+        
+        [Then(@"Response with a new token")]
+        public void ThenResponseWithSuccessMessage()
+        {
+            response = JsonConvert.DeserializeObject<Response>(_restResponse.Content);
+            Assert.That(response.token, Is.Not.Null);
+        }
+        
+        [Then(@"Response with message about already assigned email")]
+        public void ThenResponseWithMessageAboutAlreadyAssignedEmail()
+        {
+            response = JsonConvert.DeserializeObject<Response>(_restResponse.Content);
+            Assert.That(response.error, Is.EqualTo(""));
+        }
+        
+        [Then(@"Response with message about password")]
+        public void ThenResponseWithMessageAboutPassword()
+        {
+            response = JsonConvert.DeserializeObject<Response>(_restResponse.Content);
+
+            Assert.That(response.error, Is.EqualTo("Wprowadzone hasło jest nieprawidłowe!"));
+        }
+        
+        [Then(@"Response with message about incorrect email based on (.*)")]
+        public void ThenResponseWithMessageAboutIncorrectEmail(string kindOfMistake)
+        {
+            response = JsonConvert.DeserializeObject<Response>(_restResponse.Content);
+            if (kindOfMistake != "too long") Assert.That(response.validationError[0].newUserEmail, Is.EqualTo("Adres e-mail został wprowadzony niepoprawnie!"));
+            else
+            {
+                Assert.That(response.validationError[0].newUserEmail, Is.EqualTo("Wprowadzony adres e-mail jest za długi!"));
+            }
+        }
+        
+        [Then(@"Response with message about incorrect password based on (.*)")]
+        public void ThenResponseWithMessageAboutIncorrectPassword(string kindOfMistake)
+        {
+            response = JsonConvert.DeserializeObject<Response>(_restResponse.Content);
+            if(kindOfMistake == "which is too short" || kindOfMistake == "empty") Assert.That(response.validationError[0].userPassword, Is.EqualTo("Hasło jest za krótkie!"));
+            else if (kindOfMistake == "'which is too long'") Assert.That(response.validationError[0].userPassword, Is.EqualTo("Hasło jest za długie!"));
+        }
+
+        [Then(@"Response with error message about authorization")]
+        public void ThenResponseWithErrorMessageAboutAuthorization()
+        {
+            response = JsonConvert.DeserializeObject<Response>(_restResponse.Content);
+            Assert.That(response.error, Is.EqualTo("Nie udało się przeprowadzić procesu uwierzytelniania!"));
+        }
+    }
+}
